411a82aeeba75cdb2a92914057c594a75df41eb0
neelesh <neeleshvinchoorkar@gmail.com>
2024-10-11 11:30:29 +0530

    puppeteer error handled + limit param added in api

--- controllers/scraperController.js
+++ controllers/scraperController.js
@@ -1,11 +1,16 @@
 const tryCatch = require("../utils/tryCatch");
-const scraper = require("../utils/scraper")
+const scraper = require("../utils/scraper");
+const customError = require("../utils/customError");
 
-const scrapeController = tryCatch(async(req, res, next) =>{
-    const {page} = req.query;
+const scrapeController = tryCatch(async (req, res, next) => {
+    let { page, limit } = req.query;
+    
+    if(limit && !parseInt(limit)) return next(new customError("Please pass a valid limit parameter!"));
+    if(!limit) limit = -1;
 
-    const reviews = await scraper(page);
-    return res.status(200).json({ reviews_count: reviews?.length, reviews})
+    const reviews = await scraper(page, parseInt(limit));
+
+    return res.status(200).json({ reviews_count: reviews?.length, reviews })
 
 })
-module.exports = {scrapeController}
+module.exports = { scrapeController }
